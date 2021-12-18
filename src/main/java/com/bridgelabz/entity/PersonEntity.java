af1c9914a1c1a5061516352e792077298c0c9507
Ashok Koda <a.koda59@gmail.com>
2021-12-18 15:15:27 +0530

    Create Address and Person entities

---
+++ src/main/java/com/bridgelabz/entity/PersonEntity.java
@@ -0,0 +1,57 @@
+package com.bridgelabz.entity;
+
+public class PersonEntity {
+
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String email;
+	private AddressEntity address;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber=" + phoneNumber + ", email="
+				+ email + ", address=" + address + "]";
+	}
+
+	public AddressEntity getAddress() {
+		return address;
+	}
+
+	public void setAddress(AddressEntity address) {
+		this.address = address;
+	}
+}
