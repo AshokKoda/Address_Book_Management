0dea2d0eb50d1c54e3be83cba0e2ac579ce6fa69
Ashok Koda <a.koda59@gmail.com>
2021-12-18 15:07:44 +0530

    Welcome to address book

---
+++ src/main/java/com/bridgelabz/Main/AddressBookManagement.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Main;
+
+/*
+ * Address Book Management
+ */
+public class AddressBookManagement {
+
+	public static void main(String[] args) {
+		System.out.println("-----Welcome To Address Book Management------");
+
+	}
+
+}
